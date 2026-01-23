4cc780bcaeea0790f533779a09cf6f3884b8b20c
Nguyen Van Tien <nguyenvantienthuanan12b3@gmail.com>
2026-01-23 18:12:52 +0700

    Add basic arithmetic operations for Back-End

---
+++ back-end/lesson/section-5/Arithmetic.php
@@ -0,0 +1,21 @@
+<?php
+$a = 15;
+$b = 4;
+$total = $a + $b;
+echo "Tổng: {$a} + {$b} = ".$total."<br>";
+echo "Hiệu: ".$a - $b ."<br>";
+echo "Nhân: ".$a * $b ."<br>";
+echo "Chia: ".$a / $b ."<br>";
+echo "Số dư: ".$a % $b ."<br>";
+echo "Luỹ Thừa: ".$a ** $b ."<br>";
+$price = 100;
+$tax = 10;
+$total_1 = $price + $tax;
+echo $total_1;
+
+$unitPrice = 50;
+$quantity = 3;
+$totalCost = $unitPrice * $quantity;
+echo "<br>";
+echo $totalCost;
+?>
